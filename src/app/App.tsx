fc527b0e563081e134b02781429006ede2a5d15c
ShabanovIV <react.dev.shabanov@mail.ru>
2025-12-15 15:14:51 +0500

    test: add vscode jest settings
      - add VSCode settings for Jest runner
      - add data-testid="app-root" for App tests

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import s from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div className={s.root}>
+    <div className={s.root} data-testid="app-root">
       <h1>Landing-Box</h1>
       <p>Стартовый каркас работает ✅</p>
     </div>
